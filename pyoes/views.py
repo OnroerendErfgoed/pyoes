3eab2ad5335dee04826bf3f3e68b66addd059420
Koen Van Daele <koen_van_daele@telenet.be>
2013-08-13 16:43:03 +0200

    Toevoegen van view voor vinden.

--- pyoes/views.py
+++ pyoes/views.py
@@ -4,3 +4,7 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/home.jinja2')
 def home(request):
     return {}
+
+@view_config(route_name='vinden', renderer='templates/vinden.jinja2')
+def vinden(request):
+    return {}
