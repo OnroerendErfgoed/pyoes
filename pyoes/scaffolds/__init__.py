c263c21928e45ef5469f073a817100f35abe9b64
Koen Van Daele <koen_van_daele@telenet.be>
2013-08-13 16:33:10 +0200

    Toevoegen van een pyoes scaffold.

---
+++ pyoes/scaffolds/__init__.py
@@ -0,0 +1,6 @@
+from pyramid.scaffolds import PyramidTemplate
+
+class PyoesTemplate(PyramidTemplate):
+    _template_dir = 'pyoes_scaffold'
+    summary = 'Een scaffold om een pyramid project uit te breiden met OE stijl\
+    bestanden op basis van sass en compass.'
---
+++ pyoes/scaffolds/pyoes_scaffold/+package+/static/config.rb
@@ -0,0 +1,29 @@
+require 'zurb-foundation'
+# Require any additional compass plugins here.
+
+require 'font-icons';
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+fonts_dir = "css/fonts"
+images_dir = "img"
+javascripts_dir = "js"
+extensions_dir = "extensions"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
