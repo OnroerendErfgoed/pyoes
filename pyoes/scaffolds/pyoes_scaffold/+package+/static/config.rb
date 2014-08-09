b8a08a9f07a7b8a44040a0042be1efffc5c5fdb2
Koen Van Daele <koen_van_daele@telenet.be>
2014-08-09 14:15:08 +0200

    Update config.rb

--- pyoes/scaffolds/pyoes_scaffold/+package+/static/config.rb
+++ pyoes/scaffolds/pyoes_scaffold/+package+/static/config.rb
@@ -1,13 +1,10 @@
-require 'zurb-foundation'
-# Require any additional compass plugins here.
-
-require 'font-icons';
+# Require any additional compass plugins here.                                  
+add_import_path "bower_components/foundation/scss" 
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
-fonts_dir = "css/fonts"
+sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 extensions_dir = "extensions"
