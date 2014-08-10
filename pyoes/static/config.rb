1e679831fe7984584b47f52d5df40107f9952fd9
Koen Van Daele <koen_van_daele@telenet.be>
2014-08-10 21:30:38 +0200

    Update pyoes demo.

--- pyoes/static/config.rb
+++ pyoes/static/config.rb
@@ -1,17 +1,13 @@
-require 'zurb-foundation'
-# Require any additional compass plugins here.
-
-require 'font-icons';
-
+# Require any additional compass plugins here.                                  
+add_import_path "bower_components/foundation/scss" 
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-fonts_dir = "css/fonts"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
-extensions_dir="extensions"
+extensions_dir = "extensions"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
