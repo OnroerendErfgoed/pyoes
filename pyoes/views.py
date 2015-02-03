cb2c92a2e85956a2e2bdbcd2fe6b7d84529d9114
Maarten Taeymans <maarten.taeymans@geosolutions.be>
2015-02-03 09:58:23 +0100

    updated to erfgoedstijl

--- pyoes/views.py
+++ pyoes/views.py
@@ -13,14 +13,90 @@ def header_footer(request):
 def grids(request):
     return {}
 
-@view_config(route_name='beeldbank', renderer='templates/beeldbank.jinja2')
+@view_config(route_name='beeldbank', renderer='templates/beeldbank/beeldbank.jinja2')
 def beeldbank(request):
     return {}
 
-@view_config(route_name='beeldbank-detail', renderer='templates/beeldbank-detail.jinja2')
+@view_config(route_name='beeldbank-detail', renderer='templates/beeldbank/beeldbank-detail.jinja2')
 def beeldbank_detail(request):
     return {}
 
-@view_config(route_name='inventaris', renderer='templates/inventaris.jinja2')
+@view_config(route_name='beeldbank-zoeken', renderer='templates/beeldbank/beeldbank-uitgebreid-zoeken.jinja2')
+def beeldbank_zoeken(request):
+    return {}
+
+@view_config(route_name='inventaris', renderer='templates/inventaris/inventaris.jinja2')
 def inventaris(request):
+    return {}
+
+@view_config(route_name='inventaris-article', renderer='templates/inventaris/inventaris-article.jinja2')
+def inventaris_article(request):
+    return {}
+
+@view_config(route_name='inventaris-articleID', renderer='templates/inventaris/inventaris-articleID.jinja2')
+def inventaris_articleID(request):
+    return {}
+
+@view_config(route_name='inventaris-articleIDv2', renderer='templates/inventaris/inventaris-articleIDv2.jinja2')
+def inventaris_articleIDv2(request):
+    return {}
+
+@view_config(route_name='inventaris-articleIDfull', renderer='templates/inventaris/inventaris-articleIDfull.jinja2')
+def inventaris_articleIDfull(request):
+    return {}
+
+@view_config(route_name='inventaris-articleIDfullv2', renderer='templates/inventaris/inventaris-articleIDfullv2.jinja2')
+def inventaris_articleIDfullv2(request):
+    return {}
+
+@view_config(route_name='inventaris-articleIDfullv3', renderer='templates/inventaris/inventaris-articleIDfullv3.jinja2')
+def inventaris_articleIDfullv3(request):
+    return {}
+
+@view_config(route_name='inventaris-zoekoverzicht', renderer='templates/inventaris/inventaris-zoekoverzicht.jinja2')
+def inventaris_zoekoverzicht(request):
+    return {}
+
+@view_config(route_name='icons', renderer='templates/icons.jinja2')
+def icons(request):
+    return {}
+
+@view_config(route_name='blokken', renderer='templates/blokken.jinja2')
+def blokken(request):
+    return {}
+
+@view_config(route_name='typo', renderer='templates/typo.jinja2')
+def typo(request):
+    return {}
+
+@view_config(route_name='navigation', renderer='templates/navigation.jinja2')
+def navigation(request):
+    return {}
+
+@view_config(route_name='colors', renderer='templates/colors.jinja2')
+def colors(request):
+    return {}
+
+@view_config(route_name='elements', renderer='templates/elements.jinja2')
+def elements(request):
+    return {}
+
+@view_config(route_name='images', renderer='templates/images.jinja2')
+def images(request):
+    return {}
+
+@view_config(route_name='article', renderer='templates/article.jinja2')
+def article(request):
+    return {}
+
+@view_config(route_name='article9-3', renderer='templates/article9-3.jinja2')
+def article93(request):
+    return {}
+
+@view_config(route_name='geoportaal', renderer='templates/geoportaal/geoportaal.jinja2')
+def geoportaal(request):
+    return {}
+
+@view_config(route_name='pinpoints', renderer='templates/pinpoints.jinja2')
+def pinpoints(request):
     return {}
