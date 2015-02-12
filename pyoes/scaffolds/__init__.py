570141ce023bbcd41e189738ae53a72b79463ff0
Vanderhaegen Cedrik <cedrik.vanderhaegen@geosolutions.be>
2015-02-12 11:30:31 +0100

    create admin scaffold

--- pyoes/scaffolds/__init__.py
+++ pyoes/scaffolds/__init__.py
@@ -10,3 +10,9 @@ class Pyoes030Template(PyramidTemplate):
     _template_dir = 'pyoes_0.3.0'
     summary = 'Een scaffold om een pyramid project uit te breiden met OE stijl\
     bestanden op basis van sass en compass.'
+
+
+class PyoesAdmin030Template(PyramidTemplate):
+    _template_dir = 'pyoes_admin_0.3.0'
+    summary = 'Een scaffold om een de admin interface van een pyramid project uit te breiden met OE stijl\
+    bestanden op basis van sass en compass.'
