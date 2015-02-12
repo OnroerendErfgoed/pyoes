6aecd2bf80d75feb21bde8f4878812ea02261439
Maarten Taeymans <maarten.taeymans@geosolutions.be>
2015-02-12 15:26:24 +0100

    pyoes issues #13 #14 #15 #16

--- pyoes/scaffolds/__init__.py
+++ pyoes/scaffolds/__init__.py
@@ -4,9 +4,3 @@ class PyoesTemplate(PyramidTemplate):
     _template_dir = 'pyoes_scaffold'
     summary = 'Een scaffold om een pyramid project uit te breiden met OE stijl\
     bestanden op basis van sass en compass.'
-
-
-class Pyoes030Template(PyramidTemplate):
-    _template_dir = 'pyoes_0.3.0'
-    summary = 'Een scaffold om een pyramid project uit te breiden met OE stijl\
-    bestanden op basis van sass en compass.'
