8964081eec32a98755cf410afaf6257c68310af1
Maarten Taeymans <maarten.taeymans@geosolutions.be>
2015-02-24 11:06:07 +0100

    fix for sticky footer

--- pyoes/static/js/app.js
+++ pyoes/static/js/app.js
@@ -6,3 +6,20 @@ $(document).foundation();
 $('#mobile-menu-select').change(function() {
 	window.location = $(this).val();
 });
+
+/*
+// Function to stick footer to the bottom of page
+// -- not working 100% becauce of 'vlaanderen' banners
+$(document).ready(function() {
+    var footer = $("#footer-bottom");
+    var pos = footer.position();
+    var height = $(window).height();
+    height = height - pos.top;
+    height = height - footer.height();
+    height = height - 45 * 2; // Vlaanderen banners
+    if (height > 0) {
+        footer.css({
+            'margin-top': height + 'px'
+        });
+    }
+});*/
