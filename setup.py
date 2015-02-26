3d1fc672d6807cc7b90912f70d17d38259f36528
Klaas Millet <klaas.millet@rwo.vlaanderen.be>
2015-02-26 11:23:04 +0100

    changes and setup updated

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = [
 
 setup(name='pyoes',
 
-      version='0.3.1',
+      version='0.3.2',
       description='Algemene onroerenderfgoed stijl (gebaseerd op de Vlaamse huisstijl) voor pyramid',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
