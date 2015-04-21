a05475b91250246daddf6779f5dbe3f92760dabd
Maarten Taeymans <maarten.taeymans@geosolutions.be>
2015-04-21 13:55:28 +0200

    resizeable title in header

--- pyoes/static/js/app.js
+++ pyoes/static/js/app.js
@@ -7,20 +7,20 @@ $('#mobile-menu-select').change(function() {
 	window.location = $(this).val();
 });
 
-/*
-// Function to stick footer to the bottom of page
-// -- not working 100% becauce of 'vlaanderen' banners
-*/
-$(document).ready(function() {
-    var footer = $("#footer-bottom");
-    var pos = footer.position();
-    var height = $(window).height();
-    height = height - pos.top;
-    height = height - footer.height();
-    height = height - 45 * 2; // Vlaanderen banners
-    if (height > 0) {
-        footer.css({
-            'margin-top': height + 'px'
-        });
-    }
+// Function that resizes the title to a appropriate size
+$( document ).ready(function () {
+  var element = $('.logo-block');
+  resize_title(element);
 });
+
+function resize_title(element){
+  var title = element.find('h1')[0];
+  var span = element.find('div.logo-block-title')[0];
+  var fontsize = $(title).css('font-size');
+  $(title).css('font-size', parseFloat(fontsize) - 1);
+  var padding = $(title).css('padding-top');
+  $(title).css('padding-top', parseFloat(padding) + 0.5);
+  if ($(title).width() > $(span).width()){
+    resize_title(element);
+  }
+};
