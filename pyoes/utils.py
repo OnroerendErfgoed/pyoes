24b78dc512e8f41e4ea56d92d591d5f949499532
Maarten Taeymans <maarten.taeymans@geosolutions.be>
2015-05-27 13:21:38 +0200

    datetime_format_filter

--- pyoes/utils.py
+++ pyoes/utils.py
@@ -2,7 +2,7 @@
 '''
 Contains utility methods.
 '''
-
+from dateutil import tz, parser
 
 def set_attr_filter(target, key, value):
     '''
@@ -10,3 +10,18 @@ def set_attr_filter(target, key, value):
     '''
     target[key] = value
     return target
+
+def datetime_format_filter(value, formatation='%d-%m-%Y %H:%M'):    #pragma NO COVER
+    """
+    Jinja2 filter om een datetime object af te drukken in een template.
+    Deze functie wordt momenteel nog niet gebruikt binnen adviezen
+
+    :param value:
+    :param formatation:
+    :return:
+    """
+    try:
+        return value.strftime(formatation)
+    except AttributeError:
+        date = parser.parse(value)
+        return date.strftime(formatation)
