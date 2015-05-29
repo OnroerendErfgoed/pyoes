cf508db0d0ff1b84d791aecb4d98dd86c606b6c0
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2015-05-29 15:18:45 +0200

    fix format_datetime_filter. Refs #42

--- pyoes/utils.py
+++ pyoes/utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 '''
-Contains utility methods.
+Contains some extra filters for Jinja2.
+
+To use these, they need to be registered in your pyramid config.
 '''
 from dateutil import tz, parser
 
@@ -11,17 +13,19 @@ def set_attr_filter(target, key, value):
     target[key] = value
     return target
 
-def datetime_format_filter(value, format='%d-%m-%Y %H:%M'):    #pragma NO COVER
+def datetime_format_filter(value, format='%d-%m-%Y %H:%M'):
     """
-    Jinja2 filter om een datetime object af te drukken in een template.
-    Deze functie wordt momenteel nog niet gebruikt binnen adviezen
+    Jinja 2 filter to print a datetime object in a template.
 
-    :param value:
-    :param format:
-    :return:
+    :param value: Eiter a :class:`datetime.datetime` or a string that can be \
+        parsed by :class:`dateutil.parser`. In case of ambiguity between the \
+        day and the month, the parser will assume the day precedes the month \
+        (ie. European way)
+    :param format: The format to print the date in.
+    :return: The formatted date, eg. `07-09-2014 20:00`.
     """
     try:
         return value.strftime(format)
     except AttributeError:
-        date = parser.parse(value)
+        date = parser.parse(value, dayfirst=True)
         return date.strftime(format)
