29876c3b8269cc0e1b0d15f78bc7097e0ac69ff7
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2018-03-22 14:04:15 +0100

    prepare release 0.4.8

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
     ]
 
 setup(name='pyoes',
-      version='0.4.7',
+      version='0.4.8',
       description='Algemene onroerenderfgoed stijl (gebaseerd op de Vlaamse huisstijl) voor pyramid',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
