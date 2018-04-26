85ce0b3b0ff739a9e9c13557432f49f13b43ca73
Yannick Laevaert <yannick.laevaert@realdolmen.com>
2018-04-26 14:25:47 +0200

    add new jinja2 filter specific for formatting fuzzy_dates (also for dates before 1900)

--- pyoes/utils.py
+++ pyoes/utils.py
@@ -37,3 +37,47 @@ def datetime_format_filter(value, format='%d-%m-%Y %H:%M'):
             return date.strftime(format)
     else:
         return ''
+
+
+def fuzzy_date_format_filter(value, format='%d-%m-%Y'):
+    """
+    Jinja 2 filter to print a fuzzy date object in a template.
+
+    :param value: Eiter a :class:`datetime.datetime` or a string that can be \
+        parsed by :class:`dateutil.parser`. In case of ambiguity between the \
+        day and the month, the parser will assume the day precedes the month \
+        (ie. European way)
+    :param format: The format to print the date in.
+    :return: The formatted date, eg. `07-09-2014 20:00`.
+    """
+    if value:
+        try:
+            return date_converter(value, format)
+        except AttributeError:
+            if value[0:4].find('-') == -1 and value[0:4].find('/') == -1:
+                date = parser.parse(value, yearfirst=True, dayfirst=False)
+            else:
+                date = parser.parse(value, dayfirst=True)
+            return date_converter(date, format)
+    else:
+        return ''
+
+
+def date_converter(date, format):
+    """
+    Dateconverter to convert date or datetime objects to strings in a specific format
+
+    Implemented because of the problems of datetime.strftime method with years before 1900
+    :param date:
+    :param format: The format to print the date in. (must be a predefined format)
+    :return: The formatted date, eg. `07-09-2014`
+    """
+    if format == '%d-%m-%Y':
+        return '{0.day:02d}-{0.month:02d}-{0.year:4d}'.format(date)
+    elif format == '%m-%Y':
+        return '{0.month:02d}-{0.year:4d}'.format(date)
+    elif format == '%Y':
+        return '{0.year:4d}'.format(date)
+    else:
+        # Default
+        return '{0.day:02d}-{0.month:02d}-{0.year:4d}'.format(date)
