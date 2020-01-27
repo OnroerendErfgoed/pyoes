c82cc2cb9d5a2d8367be207aae7cbf1b41d9fc06
Jonas Geuens <jonasgeuens@gmail.com>
2020-01-27 16:12:59 +0100

    #163 release prep

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
     ]
 
 setup(name='pyoes',
-      version='0.10.0',
+      version='0.11.0',
       description='Algemene onroerenderfgoed stijl (gebaseerd op de Vlaamse huisstijl) voor pyramid',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
