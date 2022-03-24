11fdf533dfbfe9fd3b15d1a5dad25fcca5ba6a62
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2022-03-24 14:35:57 +0100

    bump version naar 14alpha1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
     ]
 
 setup(name='pyoes',
-      version='0.13.0',
+      version='0.14.0-alpha.1',
       description='Algemene onroerenderfgoed stijl (gebaseerd op de Vlaamse huisstijl) voor pyramid',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
