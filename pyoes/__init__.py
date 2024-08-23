7340b356946b1c96dd61f1250bab6ad8a43b26f0
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2024-08-23 12:27:17 +0200

    hernoem home route comform andere toepassingen

--- pyoes/__init__.py
+++ pyoes/__init__.py
@@ -9,7 +9,7 @@ def main(global_config, **settings): # pragma: no cover
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('index', '/')
+    config.add_route('home', '/')
     config.add_route('burgerprofiel', '/burgerprofiel')
     config.add_route('headerlinks', '/headerlinks')
     config.add_route('grids', '/grids')
